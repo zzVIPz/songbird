aa9c0d89b42eb0821c9313cbaba824903b84ebc1
zzVIPz <viperexe@mail.ru>
2020-08-05 13:59:38 -0400

    init: start songbird task

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+class NothingPlugin {
+  constructor() {
+    this.apply = function() {};
+  }
+}
+module.exports = (env = {}) => {
+  console.log('exports', env);
+  const { mode = 'development' } = env;
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'SONGBIRD',
+        template: 'public/index.html',
+      }),
+      new CleanWebpackPlugin(),
+    ];
+
+    if (isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'main-[hash:8].css',
+        }),
+      );
+    }
+
+    if (env.analyze) {
+      plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name]-[hash:8].js',
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          enforce: 'pre',
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+        },
+
+        // Loading images
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]',
+              },
+            },
+          ],
+        },
+
+        // Loading fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
+
+        // Loading CSS
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders(),
+        },
+
+        // Loading SASS/SCSS
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [...getStyleLoaders(), 'postcss-loader', 'sass-loader'],
+        },
+      ],
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      inline: false,
+      contentBase: './dist',
+    },
+  };
+};
